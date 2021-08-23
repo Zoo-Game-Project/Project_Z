df0091987df66409fd778b764cb64eb4ef88978c
sg1111 <sg1111@empas.com>
2021-08-23 18:56:13 +0900

    맵 체인지 구현, 캐릭터 구현중...

--- Assets/Scripts/UI/Map_Change.cs
+++ Assets/Scripts/UI/Map_Change.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class Map_Change : MonoBehaviour
 {
-    public GameObject MainCamera_1;
-    public GameObject MainCamera_2;
-    public GameObject Canvas_1;
-    public GameObject Canvas_2;
+    public Camera MainCamera_1;
+    public Camera MainCamera_2;
+    public Canvas Canvas;
+    public bool IsOpen;
 
     public void Change_1()
     {
-        // 게임 일시 정지
-        Time.timeScale = 0;
-        MainCamera_1.SetActive(false);
-        Canvas_1.SetActive(false);
-        MainCamera_2.SetActive(true);
-        Canvas_2.SetActive(true);
-    }
-    public void Change_2()
-    {
-        // 게임 일시 정지
-        Time.timeScale = 0;
-        MainCamera_1.SetActive(true);
-        Canvas_1.SetActive(true);
-        MainCamera_2.SetActive(false);
-        Canvas_2.SetActive(false);
+        if (!IsOpen)
+        {
+            MainCamera_1.gameObject.SetActive(false);
+            MainCamera_2.gameObject.SetActive(true);
+            Canvas.worldCamera = MainCamera_2;
+        }
+
+        else
+        {
+            MainCamera_1.gameObject.SetActive(true);
+            MainCamera_2.gameObject.SetActive(false);
+            Canvas.worldCamera = MainCamera_1;
+        }
+        IsOpen = !IsOpen;
     }
 }
