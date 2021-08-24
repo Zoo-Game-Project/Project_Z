5016c008cb4d7553f18fa00504293d37fd7a7d52
sg1111 <sg1111@empas.com>
2021-08-24 13:42:05 +0900

    맵체인지 기능 구현.

--- Assets/Scripts/UI/Map_Change.cs
+++ Assets/Scripts/UI/Map_Change.cs
@@ -7,22 +7,28 @@ public class Map_Change : MonoBehaviour
     public Camera MainCamera_1;
     public Camera MainCamera_2;
     public Canvas Canvas;
+    public GameObject stage_1;
+    public GameObject stage_2;
     public bool IsOpen;
 
     public void Change_1()
     {
         if (!IsOpen)
         {
-            MainCamera_1.gameObject.SetActive(false);
-            MainCamera_2.gameObject.SetActive(true);
-            Canvas.worldCamera = MainCamera_2;
+            //MainCamera_1.gameObject.SetActive(false);
+            //MainCamera_2.gameObject.SetActive(true);
+            //Canvas.worldCamera = MainCamera_2;
+            stage_1.SetActive(false);
+            stage_2.SetActive(true);
         }
 
         else
         {
-            MainCamera_1.gameObject.SetActive(true);
-            MainCamera_2.gameObject.SetActive(false);
-            Canvas.worldCamera = MainCamera_1;
+            //MainCamera_1.gameObject.SetActive(true);
+            //MainCamera_2.gameObject.SetActive(false);
+            //Canvas.worldCamera = MainCamera_1;
+            stage_1.SetActive(true);
+            stage_2.SetActive(false);
         }
         IsOpen = !IsOpen;
     }
