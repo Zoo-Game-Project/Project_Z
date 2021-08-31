a8cbeea38d73f7ba863448b1a309f945df22ab6d
sg1111 <sg1111@empas.com>
2021-09-01 01:11:07 +0900

    조이스틱 손 때면 캐릭터 멈추게 수정, 인벤토리 좌표값 수정

--- Assets/Scripts/Controller/Player_Controller.cs
+++ Assets/Scripts/Controller/Player_Controller.cs
@@ -16,7 +16,15 @@ public class Player_Controller : MonoBehaviour
 
     public void Update()
     {
-        Vector2 direction = Vector2.up * fixJoy.Vertical + Vector2.right * fixJoy.Horizontal;
-        rgd2d.AddForce(direction * PlayerSpeed * Time.deltaTime);
+        if (fixJoy.Vertical != 0)
+        {
+            Vector2 direction = Vector2.up * fixJoy.Vertical + Vector2.right * fixJoy.Horizontal;
+            rgd2d.AddForce(direction * PlayerSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            rgd2d.velocity = Vector2.zero;
+        }
     }
 }
