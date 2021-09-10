31906189296f0880eba3a0689b94859928deab18
sg1111 <sg1111@empas.com>
2021-09-10 09:09:32 +0900

    벽 충돌 이슈 해결.

--- Assets/Scripts/UI/Map_Change.cs
+++ Assets/Scripts/UI/Map_Change.cs
@@ -13,7 +13,6 @@ public class Map_Change : MonoBehaviour
 
     //플레이어의 컴포넌트를 가져오기 위한 함수 (충돌시 컬라이더 종료를 위한 요소)
     public GameObject Player;
-    public GameObject Player_Check;
 
     public void Change_1()
     {
@@ -22,22 +21,25 @@ public class Map_Change : MonoBehaviour
             //MainCamera_1.gameObject.SetActive(false);
             //MainCamera_2.gameObject.SetActive(true);
             //Canvas.worldCamera = MainCamera_2;
-            
-            //충돌 시 캐릭터의 컬라이더 종료
-            Player.GetComponent<CapsuleCollider2D>().enabled = false;
-            stage_1.SetActive(false);
-            //Player_Check.GetComponent<BoxCollider2D>().enabled = true;
-            stage_2.SetActive(true);
 
-            if (WallCheck.IsWall == false)
+            if (PlayerCheck.IsWall == false)
             {
-                Debug.Log("캐릭터의 컬라이더가 다시 켜진다.");
+                Debug.Log("캐릭터의 컬라이더가 꺼지지 않는다.");
                 Player.GetComponent<CapsuleCollider2D>().enabled = true;
-                Player_Check.GetComponent<Collider2D>().enabled = false;
-                //Invoke("MoveCheck", 1f);
+                Player_Controller.IsMove = false;
+                stage_1.SetActive(false);
+                stage_2.SetActive(true);
             }
 
-            //Invoke("MoveCheck", 1f);
+            else
+            {
+                Debug.Log("캐릭터의 컬라이더가 꺼진다.");
+                Player.GetComponent<CapsuleCollider2D>().enabled = false;
+                Player_Controller.IsMove = true;
+                stage_1.SetActive(false);
+                stage_2.SetActive(true);
+            }
+
         }
 
         else
@@ -45,33 +47,26 @@ public class Map_Change : MonoBehaviour
             //MainCamera_1.gameObject.SetActive(true);
             //MainCamera_2.gameObject.SetActive(false);
             //Canvas.worldCamera = MainCamera_1;
-            
-            //충돌 시 캐릭터의 컬라이더 종료
-            Player.GetComponent<CapsuleCollider2D>().enabled = false;
-            stage_2.SetActive(false);
-            //Player_Check.GetComponent<BoxCollider2D>().enabled = true;
-            stage_1.SetActive(true);
 
-            if (WallCheck.IsWall == false)
+            if (PlayerCheck.IsWall == false)
             {
-                Debug.Log("캐릭터의 컬라이더가 다시 켜진다.");
+                Debug.Log("캐릭터의 컬라이더가 꺼지지 않는다.");
                 Player.GetComponent<CapsuleCollider2D>().enabled = true;
-                Player_Check.GetComponent<Collider2D>().enabled = false;
-                //Invoke("MoveCheck", 1f);
+                Player_Controller.IsMove = false;
+                stage_2.SetActive(false);
+                stage_1.SetActive(true);
             }
 
-            //Invoke("MoveCheck", 1f);
+            else
+            {
+                Debug.Log("캐릭터의 컬라이더가 꺼진다.");
+                Player.GetComponent<CapsuleCollider2D>().enabled = false;
+                Player_Controller.IsMove = true;
+                stage_2.SetActive(false);
+                stage_1.SetActive(true);
+            }
+
         }
         IsOpen = !IsOpen;
-    }
-    
-    void MoveCheck()
-    {
-        if (Player_Controller.IsMove == false)
-        {
-            Debug.Log("캐릭터의 컬라이더가 다시 켜진다.");
-            Player.GetComponent<CapsuleCollider2D>().enabled = true;
-            //Player_Check.GetComponent<Collider2D>().enabled = false;
-        }
     }
 }
