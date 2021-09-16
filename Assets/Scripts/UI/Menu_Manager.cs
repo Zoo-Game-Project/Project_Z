604de44de79e48f93c72403dcc6b4a53e77bbe27
sg1111 <sg1111@empas.com>
2021-09-17 03:42:08 +0900

    UI 리소스 수정, 팝업 기능 구현

--- Assets/Scripts/UI/Menu_Manager.cs
+++ Assets/Scripts/UI/Menu_Manager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Menu_Manager : MonoBehaviour
 {
     public GameObject PauseMenuPanel;
+    public GameObject ResetMenuPanel;
+    public GameObject Map_1;
+    public GameObject Map_2;
+    [SerializeField] Rigidbody2D playerRigid;
+    [SerializeField] Transform tf_OriginPos;
 
     public void Pause()
     {
@@ -13,14 +18,34 @@ public class Menu_Manager : MonoBehaviour
         PauseMenuPanel.SetActive(true);
     }
 
+    public void ResetMono()
+    {
+        // 다시하기 메뉴
+        Time.timeScale = 0;
+        ResetMenuPanel.SetActive(true);
+    }
+
+    public void reset()
+    {
+        // 다시하기
+        Time.timeScale = 1;
+        playerRigid.gameObject.transform.position = tf_OriginPos.position;
+        Map_1.SetActive(true);
+        Map_2.SetActive(false);
+        ResetMenuPanel.SetActive(false);
+    }
+
     public void Continue()
     {
+        //계속하기
         Time.timeScale = 1;
         PauseMenuPanel.SetActive(false);
+        ResetMenuPanel.SetActive(false);
     }
 
     public void GameQuit()
     {
+        //타이틀로 이동
         Application.Quit();
     }
 }
